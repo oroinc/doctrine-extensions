dab03c89ad10928f0738f0308ded1fd24c7227c0
Dmitry Khrysev <x86demon@gmail.com>
2015-09-17 20:19:15 +0300

    added concat_ws support

---
+++ src/Oro/ORM/Query/AST/Platform/Functions/Mysql/ConcatWs.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Oro\ORM\Query\AST\Platform\Functions\Mysql;
+
+use Doctrine\ORM\Query\SqlWalker;
+
+use Oro\ORM\Query\AST\Functions\String\ConcatWs as Base;
+use Oro\ORM\Query\AST\Platform\Functions\PlatformFunctionNode;
+
+class ConcatWs extends PlatformFunctionNode
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        $strings = array();
+        $stringExpressions = $this->parameters[Base::STRINGS_KEY];
+        foreach ($stringExpressions as $stringExp) {
+            $strings[] = $sqlWalker->walkStringPrimary($stringExp);
+        }
+
+        return sprintf(
+            'CONCAT_WS(%s, %s)',
+            $sqlWalker->walkStringPrimary($this->parameters[Base::SEPARATOR_KEY]),
+            implode(', ', $strings)
+        );
+    }
+}

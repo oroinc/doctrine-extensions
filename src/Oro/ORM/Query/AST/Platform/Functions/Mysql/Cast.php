d66b8ff8cfb0f4adfd6f074c9f1f5e11738e2d86
Dmytro Khrysiev <dkhrysev@oroinc.com>
2026-09-09 19:45:14 +0200

    BAP-23483: Cast DQL function: revisit how the target type is accepted and rendered
    - Changed Oro\ORM\Query\AST\Functions\Cast to accept a target type only when it matches a supported type exactly, instead of accepting any type that starts with a supported one
    - Changed Oro\ORM\Query\AST\Functions\Cast::parse() to accept a length or a precision argument only as a non-negative integer
    - Added Oro\ORM\Query\AST\Functions\Cast::splitType() that splits a target type into the type name and its length or precision arguments
    - Changed Oro\ORM\Query\AST\Platform\Functions\Mysql\Cast and Oro\ORM\Query\AST\Platform\Functions\Postgresql\Cast to map the type name and to render its length or precision separately, rejecting a type of any other shape

--- src/Oro/ORM/Query/AST/Platform/Functions/Mysql/Cast.php
+++ src/Oro/ORM/Query/AST/Platform/Functions/Mysql/Cast.php
@@ -14,18 +14,21 @@ class Cast extends PlatformFunctionNode
     {
         /** @var Node $value */
         $value = $this->parameters[DqlFunction::PARAMETER_KEY];
-        $type  = $this->parameters[DqlFunction::TYPE_KEY];
+        [$type, $arguments] = DqlFunction::splitType($this->parameters[DqlFunction::TYPE_KEY]);
 
-        $type = \strtolower($type);
         $isBoolean = $type === 'bool' || $type === 'boolean';
         if ($type === 'char') {
-            $type = 'char(1)';
+            $arguments = $arguments ?: ['1'];
         } elseif ($type === 'string' || $type === 'text' || $type === 'uuid') {
             $type = 'char';
         } elseif ($type === 'int' || $type === 'integer' || $isBoolean) {
             $type = 'signed';
         } elseif ($type === 'bigint') {
             $type = 'unsigned';
+        }
+
+        if ($arguments) {
+            $type .= '(' . \implode(', ', $arguments) . ')';
         }
 
         $expression = 'CAST(' . $this->getExpressionValue($value, $sqlWalker) . ' AS ' . $type . ')';
